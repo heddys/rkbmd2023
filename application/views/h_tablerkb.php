ae9b9665e625b7be2d05f6d87f5103767dba5dc6
heddys <heddys93@gmail.com>
2022-03-30 21:57:05 +0700

    komit

--- application/views/h_tablerkb.php
+++ application/views/h_tablerkb.php
@@ -23,7 +23,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="sidebar-mini layout-fixed" style="height: auto;">
+<body class="sidebar-mini layout-fixed" style="height: auto;> 
 <div class="wrapper">
 
   <!-- Navbar -->
