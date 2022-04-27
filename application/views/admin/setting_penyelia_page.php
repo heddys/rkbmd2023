cab64207331ffd840c92619ca89190950c598191
heddys <heddys93@gmail.com>
2022-04-27 17:05:17 +0700

    lanjutkan admin page

---
+++ application/views/admin/setting_penyelia_page.php
@@ -0,0 +1,80 @@
+<section class="content">
+      <div class="row">
+        <div class="col-12">
+          <!-- <a href="<?php echo site_url('/home/rkbform');?>" class="btn btn-primary btn-flat">Tambah Kegiatan </a>
+          <a href="#" class="btn btn-success btn-flat excel" disabled>Export Excel</a> -->
+          <hr>
+        	<div class="card">
+	            <div class="card-header">
+	              <h3 class="card-title">
+                      <center>LIST PENYELIA</center></h3>
+	            </div>
+	            <!-- /.card-header -->
+	            <div class="card-body" style="overflow-x:auto;">
+	              <table id="tabel_proses_register" class="table table-bordered table-hover ">
+	                <thead class="thead-dark">
+	                <tr>
+	                  <th><center>No.</center></th>
+	                  <th><center>Nama</center></th>
+	                  <th><center>NIP</center></th>
+	                  <th><center>Pangkat</center></th>
+	                  <th><center>Aksi</center></th>
+	                </tr>
+	                </thead>
+                  	<tbody>
+                    
+	                </tbody>
+	              </table>
+	            </div>
+	            <!-- /.card-body -->
+	          </div>
+	          <!-- /.card --> 
+	        </div>
+	        <!-- /.col -->
+	      </div>
+	      <!-- /.row -->
+
+	    </section>
+	    <!-- /.content -->
+	    	<div class="modal fade" id="detail-register">
+				<div class="modal-dialog modal-xl ">
+					<div class="modal-content">
+						<div class="modal-header">
+							<style type="text/css">.capitalize{text-transform: capitalize;}</style>
+							<div class="alert alert-success">
+								<h5><i class="icon fas fa-info"></i></span><b>Nama Komponen</b></h5>
+								<h5><p class="modal-title" id="idku"></p></h5>
+							</div>
+						</div>
+						
+						<div class="modal-body" style="overflow-x:auto;">
+							<div class="isi_body"></div>
+						</div>
+						<div class="modal-footer ">
+							<button type="button" class="btn btn-warning" data-dismiss="modal">Kembali</button>
+						</div>
+					</div>
+				</div>
+          	</div>
+			<div class="modal fade" id="modal-delete">
+				<div class="modal-dialog modal-sm-danger">
+					<div class="modal-content">
+						<div class="modal-header">
+							<style type="text/css">.capitalize{text-transform: capitalize;}</style>
+							<center><h4 class="modal-title"><i class="icon fas fa-exclamation-triangle"></i></h4></center>
+						</div>
+						<div class="modal-body">
+								<input type="hidden" id="id">
+							<center><strong>Apakah Anda Yakin Ingin Menghapus Data ? </strong></center>
+						</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-info batal" data-dismiss="modal">Batalkan</button>
+								<button type="submit" class="btn btn-danger delete" data-dismiss="modal">Ya! Hapus</button>
+							</div>
+						</div>
+						<!-- /.modal-content -->
+					</div>
+						<!-- /.modal-dialog -->
+				</div>
+		</div>
+	</div>
