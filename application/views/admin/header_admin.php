56d56f7dc05b050d8082cbf64c90384cdadd7370
heddys <heddys93@gmail.com>
2023-05-24 15:46:46 +0700

    s

--- application/views/admin/header_admin.php
+++ application/views/admin/header_admin.php
@@ -332,7 +332,7 @@
     <!-- /.sidebar -->
   </aside>
   <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
