be462f3992a0a645b6cb8d327948d56ca8f61bf5
hakim24-backend <mtaufiqalhakim24@gmail.com>
2023-07-17 23:56:10 +0700

    asd

--- application/views/form_page.php
+++ application/views/form_page.php
@@ -93,7 +93,7 @@
                   <tbody>
                     <tr>
                       <td width="70%">
-                        <form role="form" action="<?php echo base_url();?>index.php/form_inv/index/2" method="post">
+                        <form role="form" action="<?php echo base_url();?>index.php/form_inv/index/<?php echo $kib_apa; ?>" method="post">
                           <div class="col-md-5">
                           <select class="form-control select2" id="select_lokasi" name="select_lokasi" required="required">
                                     <option selected value="">Cari Berdasarkan Lokasi</option>
@@ -111,7 +111,7 @@
                       <td>
                       </td>
                       <td width="50%">
-                      <form role="form" action="<?php echo base_url();?>index.php/form_inv/index/2" method="post">
+                      <form role="form" action="<?php echo base_url();?>index.php/form_inv/index/<?php echo $kib_apa; ?>" method="post">
                           <div class="input-group">
                             <input type="text"  class="form-control" name="cariregname" placeholder="Cari Berdasarkan Register atau Nama Barang" required>
                             <button class="btn btn-primary" type="submit" id="button-addon2"><i class="fa fa-search"></i></button>
@@ -164,7 +164,7 @@
 	              </table>
                 <br>
                 <b><u>Silahkan Pilih Banyak Data Per Halaman : </b></u>
-                <form role="form" action="<?php echo site_url();?>/form_inv/index/2" method="post">
+                <form role="form" action="<?php echo site_url();?>/form_inv/index/<?php echo $kib_apa; ?>" method="post">
                   <select class="form-control select_limit" name="limit" id="limit">
                     <option value="10">10</option>
                     <option value="20">20</option>
